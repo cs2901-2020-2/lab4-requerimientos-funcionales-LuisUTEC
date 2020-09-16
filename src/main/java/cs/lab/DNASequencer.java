1f462cb0119102641278efb7f2ab7bec9e3c424a
Luis Cordero Pinela <luis.cordero@utec.edu.pe>
2020-09-16 11:08:47 -0500

    .

--- src/main/java/cs/lab/DNASequencer.java
+++ src/main/java/cs/lab/DNASequencer.java
@@ -3,24 +3,32 @@ package cs.lab;
 import java.util.List;
 import java.util.logging.Logger;
 
-public class  DNASequencer {
+
+
+public class DNASequencer {
+
 
     static final Logger logger = Logger.getLogger(DNASequencer.class.getName());
+
     public DNASequencer() {
         logger.info("Starting sequencer...");
+
     }
 
     public String calculate(List<String> part){
         String semicadena = "";
-        
-        for (int i = 0; i < part.size(); i++){
+        String cadenatotal = part.get(0);
+
+        for (int i = 1; i < part.size(); i++){
             for (int j = 0; j < part.get(i).length(); j++ ){
-                if ((part.get(i).substring(j).equals(part.get(i+1).substring(0,j))){
+                if (cadenatotal.substring(j).equals(part.get(i).substring(0,j))){
                     semicadena = part.get(i).substring(j);
+                    cadenatotal = cadenatotal.replace(semicadena, part.get(i+1));
                     break;
                 }
             }
         }
-        return "AGATTACAGA";
+        return cadenatotal;
     }
+
 }
